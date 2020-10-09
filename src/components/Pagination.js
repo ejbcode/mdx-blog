7cb651e448a6cb70bbaac64966a53d5776fa1059
Ernesto B <ejbcode@gmail.com>
2020-10-09 02:53:00 -0300

    dswds4d

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { PaginationElement, PaginationWrapper } from '../elements';
 
 export const Pagination = ({ isFirst, isLast, prevPage, nextPage }) => (
-  <PaginationWrapper isFirst={isFirst}>
+  <PaginationWrapper isFirst={isFirst} isLast={isLast}>
     <PaginationElement to={prevPage}>previous</PaginationElement>
     <PaginationElement to={nextPage}>Next</PaginationElement>
   </PaginationWrapper>
